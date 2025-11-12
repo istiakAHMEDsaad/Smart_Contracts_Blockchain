276d358b1ea56909274958fca838f14358919848
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-13 02:11:34 +0600

    initial project and setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import { Navbar, Welcome, Footer, Services, Transaction } from "./components";
+
+const App = () => {
+  return <div className="min-h-screen">
+    <div className="gradient"></div>
+  </div>;
+};
+
+export default App;
