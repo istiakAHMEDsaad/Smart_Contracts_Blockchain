449020069c1ce255041bd1dabb301e5581491e95
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-19 21:26:47 +0600

    image & resource added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import { Navbar, Welcome, Footer, Services, Transaction } from "./components";
+import { Navbar, Welcome, Footer, Services, Transaction } from './components';
 
 const App = () => {
-  return <div className="min-h-screen">
-    <div className="gradient"></div>
-  </div>;
+  return (
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome '>
+        <Navbar />
+        <Welcome />
+      </div>
+
+      <Services />
+      <Transaction />
+      <Footer /> 
+    </div>
+  );
 };
 
 export default App;
