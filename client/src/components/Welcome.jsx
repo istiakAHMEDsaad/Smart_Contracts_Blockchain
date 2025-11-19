f705b81f0b8dca8ed66ec79ec3a750a4a80a7fce
istiakAHMEDsaad <istik2020@gmail.com>
2025-11-19 23:29:33 +0600

    wonderful ethereum card done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -50,11 +50,22 @@ const Welcome = () => {
             <div className='flex justify-between flex-col w-full h-full'>
               <div className='flex justify-between items-start'>
                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
-                  <SiEthereum fontSize={21} color='#fff'/>
+                  <SiEthereum fontSize={21} color='#fff' />
                 </div>
+                <BsInfoCircle fontSize={17} color='#fff' />
+              </div>
+
+              {/* para */}
+              <div>
+                <p className='text-white font-light text-sm'>Address</p>
+                <p className='text-white font-semibold text-lg mt-1'>
+                  Ethereum
+                </p>
               </div>
             </div>
           </div>
+
+          
         </div>
       </div>
     </div>
