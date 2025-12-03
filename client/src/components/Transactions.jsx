5ff3886ddf1ba3de94578956541b9f22d834940c
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-03 23:20:43 +0600

    shorten url

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useContext } from 'react';
+import { TransactionContext } from '../context/TransactionContext';
+import dummyData from '../utils/dummyData';
+import { shortenAddress } from '../utils/shortenAddress';
 
 const Transactions = () => {
+  const { currentAccount } = useContext(TransactionContext);
   return (
-    <div>Transactions</div>
-  )
-}
+    <div className='flex justify-center items-center w-full 2xl:px-20 gradient-bg-transactions'>
+      <div className='flex flex-col md:p-12 py-12 px-4'>
+        {currentAccount ? (
+          <h3 className='text-white text-3xl text-center my-2'>
+            Latest Transaction
+          </h3>
+        ) : (
+          <h3 className='text-white text-3xl text-center my-2'>
+            Connect Your Account to see the latest transaction
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default Transactions
+export default Transactions;
